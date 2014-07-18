37fc818739a96cec94b73584641463164f2d781b
kupol <pkulakovsky@gmail.com>
2014-07-18 08:31:24 +0300

    Initial commit

---
+++ src/main/java/ua/in/kupol/testpullrequests/Main.java
@@ -0,0 +1,10 @@
+package ua.in.kupol.testpullrequests;
+
+/**
+ * Created by dn on 18.07.2014.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Master");
+    }
+}
