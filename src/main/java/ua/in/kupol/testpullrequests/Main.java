0aca4740f6edfcc193e27b5d076c44ab105c8ff2
kupol <pkulakovsky@gmail.com>
2014-07-18 08:51:25 +0300

    add: my massages

--- src/main/java/ua/in/kupol/testpullrequests/Main.java
+++ src/main/java/ua/in/kupol/testpullrequests/Main.java
@@ -5,6 +5,6 @@ package ua.in.kupol.testpullrequests;
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("pull requests brunch");
+        System.out.println("pull requests brunch by kupolua");
     }
 }
